4b4f82d586ca267e4b6eb715788507d74048fc5e
bhagyashri <brahangdale@theappwelt.com>
2024-12-22 15:49:12 +0530

    write code for sum of one or 2 numbers

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -1,5 +1,7 @@
 class Calculator
   def add(numbers)
-    return 0 if numbers.empty?
+    return 0 if numbers.empty? #returns 0 for an empty string
+
+    # numbers.split(',').map(&:to_i).sum
   end
 end
