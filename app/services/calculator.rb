c052edfcf4fddbc0cc93fee8018cf067e1035e85
bhagyashri <brahangdale@theappwelt.com>
2024-12-22 16:01:53 +0530

    rspec and code for custorm delimeter

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,7 +2,15 @@ class Calculator
   def add(numbers)
     return 0 if numbers.empty? #returns 0 for an empty string
 
+    #custom delimeter
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiter = delimiter[2..-1]
+      numbers = numbers.gsub(delimiter, ",")
+    end
+
     numbers = numbers.gsub("\n", ",")  # Replace new lines with commas
+
 
     numbers.split(',').map(&:to_i).sum  #sum of two numbers
   end
--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -21,5 +21,10 @@ RSpec.describe Calculator, type: :service do
       calculator = Calculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiters' do
+      calculator = Calculator.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
