8220a6330c2f157f4601fec9d0bfe1e86bdfd86c
bhagyashri <brahangdale@theappwelt.com>
2024-12-22 15:48:30 +0530

    write rspec for return sum of one or 2 numbers

--- spec/services/calculator_spec.rb
+++ spec/services/calculator_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Calculator, type: :service do
-	describe '#add' do
+	describe '#add numbers' do
     it 'returns 0 for an empty string' do
       calculator = Calculator.new
       expect(calculator.add('')).to eq(0)
+    end
+
+    it 'returns the number for a single number' do
+      calculator = Calculator.new
+      expect(calculator.add('1')).to eq(1)
+    end
+
+    it 'returns the sum for two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add('1,5')).to eq(6)
     end
   end
 end
