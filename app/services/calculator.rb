7fccf02b77b731b39b571e50c576a19c355f9672
bhagyashri <brahangdale@theappwelt.com>
2024-12-22 15:56:16 +0530

    code for number seperate by new line

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,6 +2,8 @@ class Calculator
   def add(numbers)
     return 0 if numbers.empty? #returns 0 for an empty string
 
+    numbers = numbers.gsub("\n", ",")  # Replace new lines with commas
+
     numbers.split(',').map(&:to_i).sum  #sum of two numbers
   end
 end
